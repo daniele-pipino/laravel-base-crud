3ec055ce5b113eaeefa42c8e342b0e37f8e93cb4
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-14 13:35:59 +0200

    added softdeletes methods to the comic model

--- app/models/Comic.php
+++ app/models/Comic.php
@@ -2,9 +2,11 @@
 
 namespace App\models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['title', 'series', 'price', 'description', 'type', 'thumb', 'sale_date'];
 }
