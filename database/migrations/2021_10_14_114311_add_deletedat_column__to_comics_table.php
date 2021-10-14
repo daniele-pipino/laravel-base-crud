bc00cd042d08bd982a489e857007d061d4b45995
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-14 13:44:57 +0200

    add deleted_at table to DB

---
+++ database/migrations/2021_10_14_114311_add_deletedat_column__to_comics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDeletedatColumnToComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            //create softdeltes table 
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            //remove soft delete deleted_at table
+            $table->dropSoftDeletes();
+        });
+    }
+}
